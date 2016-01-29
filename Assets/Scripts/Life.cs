db3557747b02b642c61d3a5a3cb6abd01e66edf6
ZaruWright <samuel_gse@hotmail.com>
2016-01-30 00:37:11 +0100

    Life of players

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour {
+
+
+	/**
+	 * Public Atributtes
+	 */
+	public float life;
+	public Scrollbar scrollbar;
+
+	/**
+	 * Private Atributtes 
+	 */
+	private float maxLife;
+
+	// Use this for initialization
+	void Start () {
+		maxLife = life;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/**
+	 * If return true the character is alive
+	 * else the character is dead.
+	 */
+	bool decreaseLife (float amountToDecrease){
+		life -= amountToDecrease;
+		scrollbar.size = life / maxLife;
+
+		return life > 0;
+	}
+}
