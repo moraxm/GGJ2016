0890e512ff38277e5bc5c928b282d70c949c0b5f
ZaruWright <samuel_gse@hotmail.com>
2016-01-29 23:03:09 +0100

    Two player move with their own controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,18 @@ using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
-    public float speed = 1;
+    
+	/**
+	 * Public Atributtes
+	 */
+	public float speed = 1;
+	public bool attacker;
+
+	/**
+	 * Private Atributtes
+	 */
+	private float h;
+	private float v;
 
     Rigidbody m_rigidBody;
 	// Use this for initialization
@@ -15,9 +26,14 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
+		
+		if (attacker) {
+			h = Input.GetAxis ("HorizontalWASD");
+			v = Input.GetAxis ("VerticalWASD");
+		} else {
+			h = Input.GetAxis ("HorizontalArrows");
+			v = Input.GetAxis ("VerticalArrows");
+		}
         
         Vector3 finalspeed = new Vector3(h * speed * Time.deltaTime, m_rigidBody.velocity.y, v *speed * Time.deltaTime);
         m_rigidBody.velocity = finalspeed;
