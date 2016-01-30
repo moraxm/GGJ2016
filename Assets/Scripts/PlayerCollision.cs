8a48a5c26116fa19f902ce7b9b4db13e9cf8762b
Manuel Mora <m.moracuesta@gmail.com>
2016-01-30 19:18:40 +0100

    Player collision bug

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -45,12 +45,6 @@ public class PlayerCollision : MonoBehaviour
 				   
             }
 
-			InventaryRune inventary = collision.collider.GetComponent<InventaryRune> ();
-			if (inventary != null) {
-				inventary.DropRune ();
-			}
-
-
         }
     }
 
