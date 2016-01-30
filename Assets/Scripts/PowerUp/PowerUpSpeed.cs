660e494c4c080e6366e8056d44a0aafe59ee0417
Pedro Pereira <pereira.pedro.h@gmail.com>
2016-01-30 15:02:03 +0100

    PowerUp Speed functionality

--- Assets/Scripts/PowerUp/PowerUpSpeed.cs
+++ Assets/Scripts/PowerUp/PowerUpSpeed.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class PowerUpSpeed : PowerUp
 {
-    [Range(0,5)]
+    [Range(0,10)]
     public float speedIncrement = 2;
 
 	public override void StartCollectable(CollectableOwner owner)
     {
         base.StartCollectable(owner);
         owner.playerController.speed *= speedIncrement;
+		Debug.Log ("Speed incremented: " + speedIncrement);
     }
 
     public override void FinishCollectable()
     {
+		if (owner != null)
+			owner.playerController.speed /= speedIncrement;
         base.FinishCollectable();
-        //owner.playerController.speed /= speedIncrement;
     }
 }
