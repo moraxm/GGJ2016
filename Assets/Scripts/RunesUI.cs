de6fd9eec0dad0dd2b6b35dc921a0c0c8ee9dea2
Manuel Mora <m.moracuesta@gmail.com>
2016-01-30 21:37:13 +0100

    Fixed scene and RunesUI

--- Assets/Scripts/RunesUI.cs
+++ Assets/Scripts/RunesUI.cs
@@ -5,8 +5,13 @@ using UnityEngine.UI;
 public class RunesUI : MonoBehaviour {
 
 	public GameObject Rune;
-	public int maxRunes = 5;
-	public GameObject[] runes;
+	public int maxRunes = 7;
+	public int separation = 30;
+
+	// For level
+	public bool nextLevel;
+	public int numRunes = 0;
+	GameObject[] runes;
 
 	// For runes
 	public bool nextRune;
@@ -20,31 +25,56 @@ public class RunesUI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
 		runes = new GameObject[maxRunes];
 		// create the Rune
 		GameObject rune = (GameObject)Instantiate(Rune, transform.position, Quaternion.identity);
 		// Assign to the parent
-		rune.transform.SetParent(this.gameObject.transform,false);
+		rune.transform.parent = this.gameObject.transform;
 		// Add to the array of runes
 		runes [numRunes] = rune;
 		// increase the num of runes
 		numRunes++;
 		// There isn't runes filled in initation
 		runesFilled = 0;
-=======
-		
->>>>>>> 5935046d4697e3377249e5e529f978cfbd78727d
 		nextRune = false;
 		previousRune = false;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (nextLevel) {
+			Vector3 position;
+			// Move all the runes to the left separation/2 units
+			for (int i = 0; i < numRunes; ++i) {
+				//Modify sprite
+				runes [i].GetComponent<Image> ().sprite = spriteRuneWithoutFill;
+
+				if (numRunes < maxRunes) {
+					// Modify position
+					position = runes [i].transform.position;
+					position.x -= separation / 2;
+					runes [i].transform.position = position;
+				}
+			}
+
+			if (numRunes < maxRunes) {
+				position = runes [numRunes - 1].transform.position;
+				position.x += separation;
+
+				GameObject rune = (GameObject)Instantiate(Rune, position, Quaternion.identity);
+				rune.transform.parent = this.gameObject.transform;
+				runes [numRunes] = rune;
+				numRunes++;
+			}
+				
+			// set again the runes sprite settings
+			runesFilled = 0;
+
+			nextLevel = false;
+		}
+
 		if (nextRune) {
-			if (runesFilled < maxRunes) {
+			if (runesFilled < numRunes) {
 				runes [runesFilled].GetComponent<Image> ().sprite = spriteRuneFilled;
 				runesFilled++;
 			}
