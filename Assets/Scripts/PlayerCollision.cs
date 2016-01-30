9aafd76c772d7ee5b4f4844e08cac34b9b419324
Manuel Mora <m.moracuesta@gmail.com>
2016-01-30 17:58:22 +0100

    Drop runes only in the slower player in collisions

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -7,6 +7,11 @@ public class PlayerCollision : MonoBehaviour
     public float collisionForce = 10;
 
     Rigidbody m_rigidBody;
+	Vector3 m_lastSpeed;
+	public Vector3 lastSpeed 
+	{
+		get { return m_lastSpeed; }
+	}
 
     public void Start()
     {
@@ -18,18 +23,23 @@ public class PlayerCollision : MonoBehaviour
     {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+			PlayerCollision rb = collision.collider.GetComponent<PlayerCollision>();
             if (rb)
             {
-                float explosionForce = collisionForce * m_rigidBody.velocity.magnitude;
-                
-                //rb.AddForce(m_rigidBody.velocity * collisionForce, ForceMode.VelocityChange);
-                //rb.AddExplosionForce(explosionForce, transform.position, 5, 0.0f, ForceMode.Impulse);
+                //float explosionForce = collisionForce * m_rigidBody.velocity.magnitude;
+				// Check wich collider has more speed
+				Debug.Log ("Other speed ("+rb.name+"): "+rb.lastSpeed.magnitude+" , my speed ( "+m_rigidBody.name+"): "+lastSpeed.magnitude);
+				if (rb.lastSpeed.magnitude < lastSpeed.magnitude) {
+					
+					// The velocity of the other player is lower than my velocity so drop rune
+					InventaryRune inventary = collision.collider.GetComponent<InventaryRune> ();
+					if (inventary != null) {
+						inventary.DropRune ();
+					} 
+				}
+				   
             }
-			InventaryRune inventary = collision.collider.GetComponent<InventaryRune> ();
-			if (inventary != null) {
-				inventary.DropRune ();
-			}
+
         }
     }
 
@@ -44,4 +54,9 @@ public class PlayerCollision : MonoBehaviour
         // Apply other rigidbody direction to us
         m_rigidBody.velocity = speed;
     }
+
+	void Update()
+	{
+		m_lastSpeed = m_rigidBody.velocity;
+	}
 }
