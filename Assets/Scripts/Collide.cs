4a8d10dbb0d9e39bae1ac4bd8768140040acbb5d
ZaruWright <samuel_gse@hotmail.com>
2016-01-30 11:50:56 +0100

    TimeBar added

---
+++ Assets/Scripts/Collide.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collide : MonoBehaviour {
+
+	public float explosionStrength = 0.3f;
+
+	private Vector3 forceVec;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "PlayerAttack" ||
+			col.gameObject.tag == "PlayerDefense")
+		forceVec = -col.gameObject.GetComponent<Rigidbody>().velocity.normalized * explosionStrength;
+		col.gameObject.GetComponent<Rigidbody> ().AddForce (forceVec,ForceMode.Acceleration);
+	}
+}
