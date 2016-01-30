5e9b392c03de2c2e33128d50a05c71c710f2caee
Manuel Mora <m.moracuesta@gmail.com>
2016-01-30 19:15:49 +0100

    SphereForce power up

---
+++ Assets/Scripts/CollisionEvents.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionEvents : MonoBehaviour {
+
+	public delegate void CollisionEventDelegate(Collision coll);
+	public CollisionEventDelegate onCollisionEnter;
+	public CollisionEventDelegate onCollisionExit;
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (onCollisionEnter != null)
+			onCollisionEnter (col);
+	}
+
+	void OnCollisionExit(Collider col)
+	{
+		if (onCollisionExit != null)
+			onCollisionExit (col);
+	}
+}
---
+++ Assets/Scripts/PowerUp/PowerUpCapsuleForce.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpCapsuleForce : PowerUp {
+	public CollisionEvents eventsOfSphereForce;
+
+	public override void StartCollectable (CollectableOwner owner)
+	{
+		base.StartCollectable (owner);
+		eventsOfSphereForce.onCollisionEnter += CheckCollision;
+	}
+
+	public override void FinishCollectable ()
+	{
+		base.FinishCollectable ();
+		eventsOfSphereForce -= CheckCollision;
+	}
+
+	void CheckCollision(Collision col)
+	{
+		//col.co
+	}
+}
